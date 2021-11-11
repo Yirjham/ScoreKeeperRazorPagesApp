b57b57c36764ceb4950afe4c6a1464258dd5db2d
Yirjham <yirjham@gmail.com>
2021-11-11 11:16:40 +0000

    Created pages for three and four players. Tested them and worked ok so far

--- ScoreKeeperRazorPagesUI/Pages/Game/FourPlayers.cshtml.cs
+++ ScoreKeeperRazorPagesUI/Pages/Game/FourPlayers.cshtml.cs
@@ -6,11 +6,11 @@ using System.Linq;
 
 namespace ScoreKeeperRazorPagesUI.Pages.Game
 {
-    public class TwoPlayersModel : PageModel
+    public class FourPlayersModel : PageModel
     {
         private readonly ScoreKeeperRazorPagesUI.Data.ScoreKeeperRazorPagesUIContext _context;
 
-        public TwoPlayersModel(ScoreKeeperRazorPagesUI.Data.ScoreKeeperRazorPagesUIContext context)
+        public FourPlayersModel(ScoreKeeperRazorPagesUI.Data.ScoreKeeperRazorPagesUIContext context)
         {
             _context = context;
         }
@@ -21,24 +21,39 @@ namespace ScoreKeeperRazorPagesUI.Pages.Game
         [BindProperty(SupportsGet = true)]
         public int ScoreSubtotalP2 { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public int ScoreSubtotalP3 { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int ScoreSubtotalP4 { get; set; }
+
         [BindProperty]
         public Player Player1 { get; set; }
 
         [BindProperty]
         public Player Player2 { get; set; }
 
+        [BindProperty]
+        public Player Player3 { get; set; }
+
+        [BindProperty]
+        public Player Player4 { get; set; }
+
         public IList<Player> Players { get; set; }
+
+
         public void OnGet()
         {
             Players = _context.Player.ToList();
             Player1 = Players[0];
             Player2 = Players[1];
+            Player3 = Players[2];
+            Player4 = Players[3];
             Player1.ScoreSubtotal = ScoreSubtotalP1;
             Player2.ScoreSubtotal = ScoreSubtotalP2;
-
-         
+            Player3.ScoreSubtotal = ScoreSubtotalP3;
+            Player4.ScoreSubtotal = ScoreSubtotalP4;
         }
-
         public IActionResult OnPost()
         {
             if (ModelState.IsValid == false)
@@ -48,8 +63,11 @@ namespace ScoreKeeperRazorPagesUI.Pages.Game
 
             Player1.UpdateRoundSubtotal();
             Player2.UpdateRoundSubtotal();
+            Player3.UpdateRoundSubtotal();
+            Player4.UpdateRoundSubtotal();
 
-            return RedirectToPage("/Game/TwoPlayers", new {ScoreSubtotalP1 = Player1.ScoreSubtotal, ScoreSubtotalP2 = Player2.ScoreSubtotal});
+
+            return RedirectToPage("/Game/FourPlayers", new { ScoreSubtotalP1 = Player1.ScoreSubtotal, ScoreSubtotalP2 = Player2.ScoreSubtotal, ScoreSubtotalP3 = Player3.ScoreSubtotal, ScoreSubtotalP4 = Player4.ScoreSubtotal });
         }
     }
 }
--- ScoreKeeperRazorPagesUI/Pages/Game/ThreePlayers.cshtml.cs
+++ ScoreKeeperRazorPagesUI/Pages/Game/ThreePlayers.cshtml.cs
@@ -6,11 +6,11 @@ using System.Linq;
 
 namespace ScoreKeeperRazorPagesUI.Pages.Game
 {
-    public class TwoPlayersModel : PageModel
+    public class ThreePlayersModel : PageModel
     {
         private readonly ScoreKeeperRazorPagesUI.Data.ScoreKeeperRazorPagesUIContext _context;
 
-        public TwoPlayersModel(ScoreKeeperRazorPagesUI.Data.ScoreKeeperRazorPagesUIContext context)
+        public ThreePlayersModel(ScoreKeeperRazorPagesUI.Data.ScoreKeeperRazorPagesUIContext context)
         {
             _context = context;
         }
@@ -21,24 +21,30 @@ namespace ScoreKeeperRazorPagesUI.Pages.Game
         [BindProperty(SupportsGet = true)]
         public int ScoreSubtotalP2 { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public int ScoreSubtotalP3 { get; set; }
+
         [BindProperty]
         public Player Player1 { get; set; }
 
         [BindProperty]
         public Player Player2 { get; set; }
 
+        [BindProperty]
+        public Player Player3 { get; set; }
+
         public IList<Player> Players { get; set; }
+
         public void OnGet()
         {
             Players = _context.Player.ToList();
             Player1 = Players[0];
             Player2 = Players[1];
+            Player3 = Players[2];
             Player1.ScoreSubtotal = ScoreSubtotalP1;
             Player2.ScoreSubtotal = ScoreSubtotalP2;
-
-         
+            Player3.ScoreSubtotal = ScoreSubtotalP3;
         }
-
         public IActionResult OnPost()
         {
             if (ModelState.IsValid == false)
@@ -48,8 +54,10 @@ namespace ScoreKeeperRazorPagesUI.Pages.Game
 
             Player1.UpdateRoundSubtotal();
             Player2.UpdateRoundSubtotal();
+            Player3.UpdateRoundSubtotal();
 
-            return RedirectToPage("/Game/TwoPlayers", new {ScoreSubtotalP1 = Player1.ScoreSubtotal, ScoreSubtotalP2 = Player2.ScoreSubtotal});
+
+            return RedirectToPage("/Game/ThreePlayers", new { ScoreSubtotalP1 = Player1.ScoreSubtotal, ScoreSubtotalP2 = Player2.ScoreSubtotal, ScoreSubtotalP3 = Player3.ScoreSubtotal });
         }
     }
 }
