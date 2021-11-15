7a919f32f542dc802af1e62593c3e8ec1d0d89cb
Yirjham <yirjham@gmail.com>
2021-11-15 23:02:53 +0000

    MVP ready

---
+++ ScoreKeeperRazorPagesUI/Pages/Instructions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScoreKeeperRazorPagesUI.Pages
+{
+    public class InstructionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
